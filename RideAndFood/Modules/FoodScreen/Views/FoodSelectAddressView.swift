5f7547553a755cb9b7edf9974c01ebdb465e1c09
Alex <sanya1444@gmail.com>
2020-11-18 16:37:16 +0200

    up

---
+++ RideAndFood/Modules/FoodScreen/Views/FoodSelectAddressView.swift
@@ -0,0 +1,9 @@
+//
+//  FoodSelectAddressView.swift
+//  RideAndFood
+//
+//  Created by Oleksandr Karpenko on 18.11.2020.
+//  Copyright © 2020 skillbox. All rights reserved.
+//
+
+import Foundation
